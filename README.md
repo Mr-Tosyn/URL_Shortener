565aba86698698614be00c45fe432069554d443c
Mr-Tosyn <51682725+Mr-Tosyn@users.noreply.github.com>
2024-03-21 18:57:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+**ALTSCHOOL CAPSTONE PROJECT**
+
+The AltSchool Capstone Project is a comprehensive educational project with various innovative features. 
+Scissor url shorten is an open source url shortener written in python. It is a simple and easy to use url shortener. 
+It is a free and open source url shortener, making them more manageable and easier to share.
+
+**Features**
+**URL Shortening:** Users can input a long URL and obtain a shortened link that redirects to the original URL.
+
+**Custom Short URLs:** The app supports custom short URLs, allowing users to personalize the shortened links.
+
+**Click Analytics:** The app tracks the number of clicks on each shortened link, providing valuable analytics on link engagement.
+
+**QR Code Generation:** The app generates QR codes for the shortened links, making it easy to share and scan.
+
+## Useful Commands
+
+# For creating Python virtual environments
+    Python -m venv venv
+    source venv/scripts/activate
+
+# FastAPI installation
+    pip install fastapi
+    pip install uvicorn
+
+# command to run the application in local
+    uvicorn main:app --reload
+
+# Install the requirements files
+    pip install -r requirements.txt
+
+# For removing python cache files
+    pyclean
+
+**Contributing**
+Contributions to the AltSchool Capstone Project and the URL Shorten Clone Python App are welcome! If you encounter any bugs, have feature requests, or would like to contribute code, please open an issue or submit a pull request to this repository. Your contributions will help improve the functionality and usability of the application.
+
+**License**
+This project is licensed under the MIT License. You are free to use, modify, and distribute the code as per the terms of the license.
---
+++ url_shortener/router.py
@@ -0,0 +1,120 @@
+import io
+import qrcode
+import os
+import urllib.parse
+import requests
+from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.staticfiles import StaticFiles 
+import urllib.parse
+import secrets
+import string
+from pydantic import ValidationError
+from fastapi import APIRouter, HTTPException
+from url_shortener.models import CreateUrlShortener, CreateUrlShortenerResponse
+from url_shortener.database import MockDBOperations
+from starlette.responses import RedirectResponse
+     
+# Create the router
+url_shortener = APIRouter()
+
+# Create the database
+mock_db_operations = MockDBOperations()
+
+
+# This function is used to generate the short url
+# Frist it validates the URL
+# returns the CreatedUrlShortenerResponse
+
+# Ensure 'static' directory exists
+if not os.path.exists('static'):
+    os.makedirs('static')
+
+# Configure the 'static' directory to serve static files
+url_shortener.mount("/static", StaticFiles(directory="static"), name="static")
+
+@url_shortener.post("/create", response_model=CreateUrlShortenerResponse)
+async def Enter_URL(shortner: CreateUrlShortener):
+    async def validate_url(url: str):
+        parsed_url = urllib.parse.urlparse(url)
+        if not all([parsed_url.scheme, parsed_url.netloc]):
+            raise HTTPException(status_code=400, detail="Invalid URL")
+        return url
+    try:
+        validated_url = await validate_url(shortner.url)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    short_url = ""
+    if validated_url:
+        short_url_length = 7
+        res = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
+                      for i in range(short_url_length))
+        short_url = str(res)
+
+        status = await mock_db_operations.add_data_to_db(url=validated_url, short_url=short_url)
+        if not status:
+            short_url = ""
+
+    return CreateUrlShortenerResponse(short_url=short_url, url=validated_url)
+
+
+# Get All urls from the database
+@url_shortener.get("/list", response_model=list[CreateUrlShortenerResponse])
+async def Fetch_History():
+    # Get the data from the database
+    data = await mock_db_operations.fetch_all_data() 
+    # Create a list of CreateUrlShortenerResponse
+    arr = []
+    # Loop through the data
+    for key, value in data.items():
+        # Add the data to the list
+        arr.append(CreateUrlShortenerResponse(short_url=key, url=value))
+    # Return the list
+    return arr
+
+
+@url_shortener.get("/generate_qr_code")
+def generate_qr_code(url:str):
+    qr = qrcode.QRCode(version=1, box_size=5, border=5)
+    qr.add_data(url)
+    qr.make()
+    img = qr.make_image(fill_color='black', back_color='white')
+    
+    # Specify the path relative to the directory of 'main.py'
+    img_path = os.path.join('static', 'shortener_qr.png')
+    img.save(img_path)
+    # return {"image_url": f"/{img_path}"}
+
+    # Return the QR code file as a response to download
+    return FileResponse(img_path, filename='shortener_qr.png')
+
+
+# Delete the url from the database
+@url_shortener.delete("/delete/{short_url}")
+async def delete_short_url(short_url : str):
+    # Delete the url from the database
+    status = await mock_db_operations.delete_data_from_db(short_url = short_url)
+    # If the url is deleted from the database, return the status
+    if status:
+        return {"message": "Successfully deleted"}
+    else:
+        # If the url is not deleted from the database, return the error message
+        return {"message": "Failed to delete"}
+
+# Redirect the user to the original url
+@url_shortener.get("/test/{short_url}")
+async def test(short_url : str):
+    # Get the url from the database
+    data = await mock_db_operations.fetch_all_data() 
+    # Check if the url exists in the database
+    if short_url in data:
+        # redirect to this url
+        url = data[short_url]
+        # return the redirect response
+        response = RedirectResponse(url=url)
+        return response
+    else:
+        # return the error message
+        return {"message": "Failed to fetch"}
+
+    
